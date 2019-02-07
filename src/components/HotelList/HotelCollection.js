3a33eea9bfd50d330cf8c0c4d5a3ee132a35aad5
Wachira <w.ndaiga@gmail.com>
2019-02-07 16:31:33 +0000

    initial asset optimizations

--- src/components/HotelList/HotelCollection.js
+++ src/components/HotelList/HotelCollection.js
@@ -22,25 +22,33 @@ class HotelCollection extends Component {
         {
          this.props.hotels.map(hotel =>
           <div key={hotel.id}>
-            <img className="img" src={this.updateImgUrl(hotel.imageurl)} alt=""/>
+            <div class="hotel-image">
+              <img className="img" src={this.updateImgUrl(hotel.imageurl)} alt=""/>
+            </div>
             <br/>
             <br/>
-              <a className="hotel-list-image-heading" href={"http://" + hotel.website} target="_blank">
-              {hotel.name}
-            </a>
-            <p className="hotel-list-image-city-and-country">
-              {hotel.city} | {hotel.country} 
-            </p> 
-            {
-              this.props.hasHotelBeenAddedToWunderList(hotel) ? 
-              <button className="hotel-list-add-to-wishlist-buttons" style={{ color: "#f2e4df", opacity: "0.5"}} >
-                Added to Wanderlist
-              </button>
-              :
-                <button className="hotel-list-add-to-wishlist-buttons" onClick={(event) => this.pulseButton(event, hotel)}>
-                  Add to Wanderlist
-                </button>
-            }
+            <div class="hotel-description">
+              <div className='background-slope'></div>
+              <div className="hotel-description-content">
+                <a className="hotel-list-image-heading" href={"http://" + hotel.website} target="_blank">
+                  {hotel.name}
+                </a>
+                <p className="hotel-list-image-city-and-country">
+                  {hotel.city} | {hotel.country} 
+                </p>
+                {
+                  this.props.hasHotelBeenAddedToWunderList(hotel)
+                  ? 
+                  <button className="hotel-list-add-to-wishlist-buttons" style={{ color: "#f2e4df", opacity: "0.5"}} >
+                    Added to Wanderlist
+                  </button>
+                  :
+                  <button className="hotel-list-add-to-wishlist-buttons" onClick={(event) => this.pulseButton(event, hotel)}>
+                    Add to Wanderlist
+                  </button>
+                }
+              </div>
+            </div>
           </div>
         )
         }
